be7ca5df97fc44b96d9ab34087f0956b8fde70af
zaddywebbuilds <zaddywebbuilds@users.noreply.github.com>
2026-09-21 10:05:15 +0000

    deploy: 1040c4061407ea6955fb72b138be4c8195ce7b2a

---
+++ _next/static/F0RH7E1gQRIF0GhMuBcXA/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/AlphaCoAsia//_next/:path+",
+        "destination": "/AlphaCoAsia/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/F0RH7E1gQRIF0GhMuBcXA/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fexpertise\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
