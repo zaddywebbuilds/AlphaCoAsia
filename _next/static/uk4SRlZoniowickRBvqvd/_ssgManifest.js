16800e0dae609a82f5d1f8c9b42de3d9e33957e2
zaddywebbuilds <zaddywebbuilds@users.noreply.github.com>
2026-09-22 20:30:14 +0000

    deploy: 10e2acaf6816335db0e6a8a945477846ed78b193

--- _next/static/uk4SRlZoniowickRBvqvd/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fcase-studies\u002F[slug]","\u002Fexpertise\u002F[slug]","\u002Findustries\u002F[slug]","\u002Fteam\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
